821744964e1232f622cf6a0b026ad91dae01af49
Luis Lopez <metfanforever@gmail.com>
2026-08-20 01:41:39 -0700

    build demo server

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const notFoundHandler = require('./handlers/404.js');
+const errorHandler = require('./handlers/500.js');
+const timeStamper = require('./middleware/stamper.js');
+
+// Route Definitions
+app.get('/', getHomePage);
+app.get('/data', timeStamper, getData);
+app.get('/bad', forceError);
+
+// Express 5 compatible 404 handler
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+// Route Handler Functions
+
+function getHomePage(req, res) {
+  res.status(200).send('Hello World');
+}
+
+function getData(req, res) {
+  let outputObject = {
+    10: 'even',
+    5: 'odd',
+    time: req.timestamp
+  };
+
+  res.status(200).json(outputObject);
+}
+
+function forceError(req, res, next) {
+  next(new Error('you messed up'));
+}
+
+function start(port) {
+  app.listen(port, () => {
+    console.log(`Server up on port ${port}`);
+  });
+}
+
+module.exports = {
+  app: app,
+  start: start
+};
